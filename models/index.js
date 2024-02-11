99b7ec623e9e48c397b2d18f7ebd5fb088c1d63e
judy_her <judith85dm@ucla.edu>
2024-02-10 16:13:24 -0800

    added associations for models in index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
 const User = require('./User');
+const Blog = require('./Blog');
 
 //user has many blogs
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
 
 //blog belongs to user
+Blog.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Blog };
