486bfeeab9181b57cb0d2963e9efedd220954104
judy_her <judith85dm@ucla.edu>
2024-02-13 18:04:33 -0800

    added get route for all blogs in homeroutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,28 @@ const { Blog, User } = require('../models');
 const withAuth = require('../utils/auth.js');
 
 //get all blogs
-router.get('/', async (req, res) => {});
+router.get('/', async (req, res) => {
+  try {
+    const blogData = await Blog.findAll({
+      inlcude: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    //make data readable by template
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+    //pass serialized blog data
+    res.render('homepage', {
+      blogs,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
 
 //get blog by id
 router.get('/blog/:id', async (req, res) => {});
